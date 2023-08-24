f6b7674fa87e69bb76f7459d8fd4135bd54f6704
David Adamson <david.adamson2304@gmail.com>
2023-08-24 18:35:19 +0200

    rename

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { createExpressEndpoints } from "@ts-rest/express";
 import { contract } from "../src/common/contract";
-import { router } from "../src/backend/router";
+import { router } from "../src/backend/main";
 
 const app = express()
   .use(express.urlencoded({ extended: false }))
