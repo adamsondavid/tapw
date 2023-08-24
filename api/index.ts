95f6db522175eec7964bea75229895d46054285c
David Adamson <david.adamson2304@gmail.com>
2023-08-24 18:16:53 +0200

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { createExpressEndpoints } from "@ts-rest/express";
-import { contract } from "../src/common/contract";
-import { router } from "../src/backend/router";
+import { contract } from "../abc/common/contract";
+import { router } from "../abc/backend/router";
 
 const app = express()
   .use(express.urlencoded({ extended: false }))
