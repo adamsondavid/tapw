6cf6364dbf663a19d7ebc01a58a15e567b08bfd0
David Adamson <david.adamson2304@gmail.com>
2023-08-28 18:16:21 +0200

    add test

--- src/client/app.vue
+++ src/client/app.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useServer } from "./composables/server";
 
 const server = useServer();
 
-async function click() {
-  const name = prompt("Whats your name?");
-  if (!name) return;
-  const { body: greeting } = await server.greet({ params: { name } });
-  alert(greeting);
-}
+const name = ref("");
+const greeting = ref("");
+
+const submit = async () => {
+  if (!name.value) return;
+  const { status, body } = await server.greet({ params: { name: name.value } });
+  if (status === 200) greeting.value = body;
+};
 </script>
 
 <template>
   <h1>Welcome to my ts-rest fullstak demo w/ vite</h1>
-  <button @click="click">Click to fetch data</button>
+  <form @submit.prevent="submit">
+    <input placeholder="The name..." v-model="name" data-cy="input" />
+    <button data-cy="submit">Submit</button>
+  </form>
+  <div data-cy="greeting">{{ greeting }}</div>
 </template>
