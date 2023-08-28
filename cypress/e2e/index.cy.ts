c962e807af382253803cf08d891f54d6fb4e2c45
David Adamson <david.adamson2304@gmail.com>
2023-08-28 18:17:34 +0200

    fix

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,8 +1,8 @@
 describe("index", () => {
   it("renders", () => {
     cy.visit("/");
-    cy.get('[data-cy="input"]').type('tapw');
+    cy.get('[data-cy="input"]').type("tapw");
     cy.get('[data-cy="submit"]').click();
-    cy.get('[data-cy="greeting"]').should('have.text', 'Hello tapw');
+    cy.get('[data-cy="greeting"]').should("have.text", "Hello tapw");
   });
 });
