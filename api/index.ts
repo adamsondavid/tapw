f37b30f832cbdb48587ebd0ed0d224e6cc6c76b8
David Adamson <david.adamson2304@gmail.com>
2024-04-12 16:01:03 +0200

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
 import { handler } from "../src/server/main";
 
 export const config = { runtime: "edge" };
-export default async (request: Request) => handler(request);
+export default handler;
--- src/main.dev.ts
+++ src/main.dev.ts
@@ -6,5 +6,3 @@ new Elysia()
   .get("/", () => new Response("hello"))
   .mount("/", handler)
   .use(node(5173));
-
-//ViteExpress.listen(app, 5173, () => console.log("dev server listening on\nhttp://localhost:5173"));
