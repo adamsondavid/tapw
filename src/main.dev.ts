6618c8b7c9f89b539b3f9472213bdb86e4f434a7
David Adamson <david.adamson2304@gmail.com>
2024-04-12 16:16:49 +0200

    fix

--- src/main.dev.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from "elysia";
-import node from "@elysiajs/node";
-import { handler } from "@/server/main";
-
-new Elysia()
-  .get("/", () => new Response("hello"))
-  .mount("/", handler)
-  .use(node(5173));
--- src/server/main.ts
+++ src/server/main.ts
@@ -2,9 +2,11 @@ import { z } from "zod";
 import { contract } from "../common/contract";
 import { initRouter } from "./router";
 import { fetchRequestHandler } from "@ts-rest/serverless/fetch";
+import { Elysia } from "elysia";
 
 const env = z.object({}).parse(process.env);
 const router = initRouter();
 
-export const handler = (request: Request) =>
-  fetchRequestHandler({ request, contract, router, options: { responseValidation: true, jsonQuery: true } });
+export const handler = new Elysia().mount((request) =>
+  fetchRequestHandler({ request, contract, router, options: { responseValidation: true, jsonQuery: true } }),
+).fetch;
