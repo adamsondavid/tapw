18f0771312cd8464e3964af30b190e608ece911f
David Adamson <david.adamson2304@gmail.com>
2024-06-06 14:08:32 +0200

    caching for heroku-like and docker deployments

--- src/main.prod.ts
+++ src/main.prod.ts
@@ -3,6 +3,10 @@ import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = initApp(process.env);
+app.use("/assets/*", async (c, next) => {
+  await next();
+  c.header("Cache-Control", "public, max-age=31556952, immutable");
+});
 app.use("*", serveStatic({ root: "./dist/static" }));
 app.notFound(() => app.request("/"));
 const server = serve({ fetch: app.fetch, port: 3000 }, ({ port }) => {
