aa36c1a57f64c05647e47f1e84b1434bfedeb8e0
David Adamson <david.adamson2304@gmail.com>
2024-11-28 21:39:18 +0100

    logging

--- src/server/router.ts
+++ src/server/router.ts
@@ -4,6 +4,9 @@ import { tsr } from "@ts-rest/serverless/fetch";
 export function initRouter(greeting?: string) {
   return tsr.router(contract, {
     async greet({ query }) {
+      console.log(`log ${query.name}`);
+      console.warn(`warn ${query.name}`);
+      console.error(`error ${query.name}`);
       return { status: 200, body: `${greeting ?? "Hello"} ${query.name}` };
     },
   });
