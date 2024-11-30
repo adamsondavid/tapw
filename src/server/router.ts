092b780e4611ad8ffe72e971decae30b28a2fcd7
David Adamson <david.adamson2304@gmail.com>
2024-11-30 02:05:21 +0100

    add errors

--- src/server/router.ts
+++ src/server/router.ts
@@ -5,6 +5,8 @@ export function initRouter(greeting?: string) {
   return tsr.router(contract, {
     async greet({ query }) {
       console.log(`log ${query.name}`);
+      console.log({ a: "b" });
+      console.log(JSON.stringify({ c: "d" }));
       console.warn(`warn ${query.name}`);
       console.error(`error ${query.name}`);
       return { status: 200, body: `${greeting ?? "Hello"} ${query.name}` };
