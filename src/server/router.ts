c4e163ee7094a27c1517b7fbf7d42b4beebaebc6
David Adamson <david.adamson2304@gmail.com>
2024-12-09 17:47:10 +0100

    add log statements

--- src/server/router.ts
+++ src/server/router.ts
@@ -4,8 +4,12 @@ import { tsr } from "@ts-rest/serverless/fetch";
 export function initRouter(greeting?: string) {
   return tsr.router(contract, {
     async greet({ query }) {
-      console.log(`log ${query.name}`);
+      console.assert(true, "hello");
+      console.assert(false, "bye");
+      console.debug("debug");
+      console.info(`info ${query.name}`);
       console.log({ a: "b" });
+      console.log({ a: "b", c: [{ d: ["a", [{ k: "e" }]] }] }, "hello", ["öö"]);
       console.log(JSON.stringify({ c: "d" }));
       console.warn(`warn ${query.name}`);
       console.error(`error ${query.name}`);
