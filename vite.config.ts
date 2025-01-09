e835b606a0a80e09544701359a8c65ebcbe80615
David Adamson <david.adamson2304@gmail.com>
2025-01-09 22:01:42 +0100

    fix: postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
 import vue from "@vitejs/plugin-vue";
 import devServer from "@hono/vite-dev-server";
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
     devServer({
