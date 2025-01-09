95886eed2b169b92c579a431342b10cab1859d4c
David Adamson <david.adamson2304@gmail.com>
2025-01-09 23:10:36 +0100

    Fix/shadcn (#299)
    * fix: deps
    
    * expose port in dockerfile
    
    * fix: postcss config
    
    * fix: tw font config
    
    * fix: shadcn config
    
    * fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
 import vue from "@vitejs/plugin-vue";
 import devServer from "@hono/vite-dev-server";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  build: {
+    outDir: "dist/static",
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
     devServer({
@@ -10,7 +21,9 @@ export default defineConfig({
       exclude: [/^(?!\/(api|openapi)).*/],
     }),
   ],
-  build: {
-    outDir: "dist/static",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
