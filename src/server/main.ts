2d3175ab0eb021e850c7e7399f23101805d9e41d
David Adamson <david.adamson2304@gmail.com>
2026-01-31 20:21:39 +0100

    use cf adapter

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,8 @@
 import { Elysia } from "elysia";
 import { createApp } from "./app";
 import { EnvSchema } from "@/server/env";
+import { CloudflareAdapter } from "elysia/adapter/cloudflare-worker";
 
-export default new Elysia({ prefix: "/server" }).use(createApp(EnvSchema.parse({ GREETING: "yo" }))).compile();
+export default new Elysia({ prefix: "/server", adapter: CloudflareAdapter })
+  .use(createApp(EnvSchema.parse({ GREETING: "yo" })))
+  .compile();
