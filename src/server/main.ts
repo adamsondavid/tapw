f6c2412d7ed32aaad14c303a7bedfa58be01f645
David Adamson <david.adamson2304@gmail.com>
2026-02-01 00:13:09 +0100

    fix

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
 import { createApp } from "./app";
 import { EnvSchema } from "@/server/env";
-import { CloudflareAdapter } from "elysia/adapter/cloudflare-worker";
+import { CloudflareAdapter, isCloudflareWorker } from "elysia/adapter/cloudflare-worker";
 
-export default new Elysia({ prefix: "/server", adapter: CloudflareAdapter })
-  .use(createApp(EnvSchema.parse(process.env)))
-  .compile();
+const app = createApp(EnvSchema.parse(process.env));
+
+let server;
+if (isCloudflareWorker()) server = new Elysia({ prefix: "/server", adapter: CloudflareAdapter }).use(app).compile();
+else server = new Elysia({ prefix: "/server" }).use(app);
+
+export default server;
